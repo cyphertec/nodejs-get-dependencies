c1e8f73dd0f528271dd0288f7c689b7da31b3cbe
中西亮太(NAKANISHI Ryouta) <nakanishi@cyphertec.co.jp>
2020-12-25 11:59:04 +0900

    log4js の使用をやめる
    Cloud Functions 上で
    Cannot find module 'log4js'
    というエラーが発生するため log4js の使用をやめる。

--- src/Log.ts
+++ src/Log.ts
@@ -1,25 +1,9 @@
-import * as log4js from 'log4js'
-
-const initLogger = (): any => {
-  log4js.configure({
-    appenders: {
-      system: { type: 'file', filename: 'system.log' }
-    },
-    categories: {
-      default: { appenders: ['system'], level: 'debug' }
-    }
-  })
-  return log4js.getLogger('system')
-}
-
 export const log = (item: string) => {
-  logger.debug(item)
+  console.log(item)
 }
 
 export const logArray = (array: any[]) => {
   array.forEach((item) => {
-    logger.debug(item)
+    console.log(item)
   })
 }
-
-const logger = initLogger()
